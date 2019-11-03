ced4817b64d6689bc7424f41feec8277c44dcd17
LIFERUIN <thetylermyers@gmail.com>
2019-11-03 13:02:07 -0500

    Liferu.in 2.0

--- js/liferuin.js
+++ js/liferuin.js
@@ -2,28 +2,9 @@ $(document).ready(function () {
     // Title Text Change
     $("#site-title").click(function () {
         $("#site-title_h1").fadeOut( function () {
-            $("#site-title_h1").text(($("#site-title_h1").text() == 'Liferuin') ? 'Boy in Bloom' : 'Liferuin').fadeIn();
+            $("#site-title_h1").text(($("#site-title_h1").text() == 'Boy In Bloom') ? 'Liferuin' : 'Boy In Bloom').fadeIn();
         })
     })
-
-    // Clock
-    function showTime(){
-        var date = new Date();
-        var h = date.getHours(); // 0 - 23
-        var m = date.getMinutes(); // 0 - 59
-        
-        h = (h < 10) ? "0" + h : h;
-        m = (m < 10) ? "0" + m : m;
-        
-        var time = h + ":" + m  + " " + "EST";
-        document.getElementById("clock-display").innerText = time;
-        document.getElementById("clock-display").textContent = time;
-        
-        setTimeout(showTime, 1000);
-        
-    }
-    
-    showTime();
 
     // Responsive Slides
     $("#slider").responsiveSlides({
